9e8669d38efaad5db83db4948b44f41a91f9687e
alexrnov <45189568+alexrnov@users.noreply.github.com>
2019-09-16 14:53:40 +0900

    Update ChangePathesForLayersFiles.py

--- ChangePathesForLayersFiles.py
+++ ChangePathesForLayersFiles.py
@@ -13,7 +13,7 @@ def changeLayerFiles(fullPath):
                 
 if __name__ == '__main__':
   #input parameters:
-	inputFolder = arcpy.GetParameterAsText(0) #folder with input files mxd 
+  inputFolder = arcpy.GetParameterAsText(0) #folder with input files mxd 
   outputFolder = arcpy.GetParameterAsText(1) #folder with output files mxd
   #sdeFile = r"D:/gis2.sde"#do when need used locale sde-file
   sdeFile = r"\\10.64.68.36\j\ElectronicArchive\Connect\gis1.sde"
