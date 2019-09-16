3984c93c54f9e56281532d557437025a62739e4d
alexrnov <45189568+alexrnov@users.noreply.github.com>
2019-09-16 14:56:07 +0900

    Create WriteLayersPathsForMXDProject.py
    Вывести на консоль все пути для слоев MXD-проекта

---
+++ WriteLayersPathsForMXDProject.py
@@ -0,0 +1,10 @@
+import arcpy, os
+def getListLayerPathesOfMXD(inputMXDFile):
+  mxd = arcpy.mapping.MapDocument(inputMXDFile)
+  for layer in arcpy.mapping.ListLayers(mxd):
+    if layer.supports("DATASOURCE"): arcpy.AddMessage(layer.dataSource)
+        
+if __name__ == '__main__':
+	inputMXDFile = arcpy.GetParameterAsText(0) 
+	if os.path.splitext(inputMXDFile)[1] == ".mxd": getListLayerPathesOfMXD(inputMXDFile)
+    else: arcpy.AddError("input file is not map document")
